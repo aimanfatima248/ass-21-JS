e4d14f442860d2d5e459c975336901b4f490b5b4
aimanfatima248 <aimanfatima65@outlook.com>
2024-03-23 03:42:26 +0500

    add ass 21

---
+++ app.js
@@ -0,0 +1,163 @@
+// Questions PDF
+// Chap 21 
+// Q1
+// var allLower = userInput.toLowerCase();
+
+// Q2
+// var x="AiMan"
+// x = x.toLowerCase();
+// console.log(x)
+
+// q3
+// var y="fatIma"
+// y = y.toUpperCase();
+// console.log(y)
+
+// Q4
+// var a = "AIMAN";
+// var y = a.toLowerCase();
+// console.log(y)
+
+// Q5
+// var a=["AIMAN","ALI","NOOR","SADAF",
+// "SABA"];
+// var index=2;
+// var y=a[index].toLowerCase();
+// console.log(y)
+
+// Q6
+// var x="Aiman";
+// var y=x.toUpperCase();
+// alert(y)
+
+// Q7
+// var city = "kaRacHi";
+
+// var firstChar = city.slice(0,1).toUpperCase();
+
+// var remainChar = city.slice(1).toLowerCase();
+//  var combineInput = firstChar + remainChar
+//  console.log(combineInput)
+
+// CHAP 22-25 "STRINGS"
+// QS1
+// var sameWords="captain";
+// var y=sameWords.slice(1,3);
+// console.log(y)
+
+// QS2
+// var sameWords="captain";
+//  var noofcharacters=sameWords.length;
+//  console.log(noofcharacters)
+
+// QS3
+// var animal="elephant";
+// var seg=animal.slice(3,7);
+// console.log(seg)
+
+// QS4
+// var aniaml="elephant";
+// var seg=aniaml.length;
+// console.log(seg)
+
+// QS5
+// var a = "HelloWorld";
+// var numberOfCharacters = a.length;
+// console.log("Number of characters:", numberOfCharacters);
+
+// var z = a.slice(1, -3);
+// console.log("Sliced string:", z);
+
+// QS6
+// var text ="To be or not to be.";
+// var indx = text.indexOf("be");
+// console.log(indx)
+
+// QS7
+// var text ="To be or not to be.";
+// var indx = text.lastIndexOf("be");
+// console.log(indx)
+
+// QS10
+// var str = "abcde";
+// var characterAtIndex2 = str.charAt(2);
+// console.log(characterAtIndex2);
+
+// QS11
+// var text = "My name is Aiman.";
+// var cha = text.slice(11,12);
+// console.log(cha);
+
+// QS12
+// var str = "My name is Aiman";
+// var x = str.slice(str.length - 1);
+// console.log(x); 
+
+// QS13
+// var input="my name is AIMAN";
+// var cha=input.slice(5,6);
+// console.log(cha)
+
+// chap 21-25 pdf
+// QS1
+// var firstName=prompt ("enter your first name");
+// var lastName=prompt ("enter your last name");
+// var fullname=(firstName+ " "+ lastName);
+// alert("welcome to page"  +" " +   fullname)
+
+// QS2
+// var phone=prompt("enter your fav mobilephone");
+// var a=phone;
+// var b= phone.length;
+// document.write("my fav phone model is"  + " "   + a + "<br>")
+// document.write("length of string:"+ b)
+
+// QS3
+// var word = "Pakistani";
+// var index = word.indexOf("n");
+
+// document.write("String" + " " + word + "<br>");
+// document.write("index of 'n':"+ index)
+
+// QS4
+// var a="hello world";
+// var b=a.lastIndexOf("l");
+// document.write("String:" + " "+ a + "<br>")
+// document.write(  "last index of 'l':" + " "+  b)
+
+// QS5
+// var a="Pakistani";
+// var b=a.charAt("3");
+// document.write("String:" + " "+ a + "<br>")
+// document.write(  "Character at index 3 is :" + " "+  b)
+
+// QS6
+// var firstName=prompt ("enter your first name");
+// var lastName=prompt ("enter your last name");
+// var fullname=firstName.concat (" "+ lastName);
+// alert("welcome to page"  +" " +   fullname)
+
+// Q7
+// var oldCity="Hyderabad"
+// var newCity=oldCity.replace("Hyder", "Islam");
+// document.write("City:" + " " + oldCity + "<br>");
+// document.write("after replacement:" + " " + newCity)
+
+// QS8
+
+// /and/g to remove more than 1 times and
+// var message = " Ali and Sami are best friends. They play cricket and football together.";
+//  var newmessage=message.replace(/and/g, "& ");
+// document.write("old message" + " " + message + "<br>");
+// document.write("after replacement" + " " + newmessage)
+
+
+
+
+
+
+
+
+
+
+
